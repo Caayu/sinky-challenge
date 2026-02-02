1b9cd5c4b517d6df75bdc1c3bd81bd96b6e40222
Caayu <henrycay@outlook.com>
2026-02-02 01:35:52 -0300

    refator(frontend): define react query cache time

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -12,8 +12,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
           queries: {
             // With SSR, we usually want to set some default staleTime
             // above 0 to avoid refetching immediately on the client
-            // staleTime: 60 * 1000
-            staleTime: 0
+            staleTime: 60 * 1000
           }
         }
       })
